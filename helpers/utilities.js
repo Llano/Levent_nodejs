bd7e8182bde092270d07d7db7fb09fe99b0c599f
Erik Andersson <dv13ean@cs.umu.se>
2016-07-09 22:01:36 +0200

    added argument parsing

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -3,7 +3,18 @@ var parseHeaderCoordinates = function(str) {
     var array = [parseFloat(coord[0]), parseFloat(coord[1])];
     return array;
 }
+var parseArguments = function() {
+    var array = {};
+    for(var i=2; i<process.argv.length; i++) {
+        if(process.argv[i].startsWith("--")) {
+            var name = process.argv[i].slice(2).split("=")[0].toUpperCase();
+            array[name] = process.argv[i].split("=").length !== 2 ? null : process.argv[i].split("=")[1];
+        }
+    }
+    return array;
+}
 
 module.exports = {
-    parseHeaderCoordinates: parseHeaderCoordinates
+    parseHeaderCoordinates: parseHeaderCoordinates,
+    parseArguments: parseArguments
 }
