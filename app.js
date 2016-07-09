03a3ccf5d9d9a2bcab1ee4e173add181efa8b719
Erik Andersson <dv13ean@cs.umu.se>
2016-07-09 20:47:31 +0200

    parsing command line arguments

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 
 
-
 // ------ Middleware ------  //
 
 //Parse all request to /api/v1/ to json
@@ -22,10 +21,50 @@ app.use('/api/v1/', router);
 var tokenRoute = require('./routes/tokenRoutes')(router);
 var eventRouter = require('./routes/eventRoutes')(router);
 
-
+// ------ Arguments ------ //
+var port,user,pw,db;
+for(var i=2; i<process.argv.length; i++) {
+  console.log(process.argv[i]);
+  if(process.argv[i].startsWith("--")) {
+    var name = process.argv[i].slice(2).split("=")[0].toUpperCase();
+    switch(name) {
+      case "PORT":
+        if(process.argv[i].split("=").length !== 2) break;
+        port = parseInt(process.argv[i].split("=")[1]);
+        break;
+      case "USER":
+        if(process.argv[i].split("=").length !== 2) break;
+        user = process.argv[i].split("=")[1];
+        break;
+      case "PASSWORD":
+        if(process.argv[i].split("=").length !== 2) break;
+        pw = process.argv[i].split("=")[1];
+        break;
+      case "DB":
+        if(process.argv[i].split("=").length !== 2) break;
+        db = process.argv[i].split("=")[1];
+        break;
+      default:
+        console.log("Unknown flag \""+name+"\"")
+    }
+  }
+}
+if(port==null) {
+  console.log("No port specified, using default port 3000");
+  port = 3000;
+}
+if(user==null) {
+  user = "username"
+}
+if(pw==null) {
+  pw = "password"
+}
+if(db==null) {
+  db = "localhost";
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port '+port+'!');
 });
--- models/dbModel.js
+++ models/dbModel.js
@@ -1,17 +1,42 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var url = 'mongodb://94.255.142.118:27017/levent';
 var connection = null;
+var url = "localhost";
 
-MongoClient.connect(url, function(err, db) {
-    db.admin().authenticate("username", "password", function(err, res) {
+for(var i=2; i<process.argv.length; i++) {
+  console.log(process.argv[i]);
+  if(process.argv[i].startsWith("--")) {
+    var name = process.argv[i].slice(2).split("=")[0].toUpperCase();
+    switch(name) {
+      case "PORT":
+        if(process.argv[i].split("=").length !== 2) break;
+        port = parseInt(process.argv[i].split("=")[1]);
+        break;
+      case "USER":
+        if(process.argv[i].split("=").length !== 2) break;
+        user = process.argv[i].split("=")[1];
+        break;
+      case "PASSWORD":
+        if(process.argv[i].split("=").length !== 2) break;
+        pw = process.argv[i].split("=")[1];
+        break;
+      case "DB":
+        if(process.argv[i].split("=").length !== 2) break;
+        url = process.argv[i].split("=")[1];
+        break;
+      default:
+        console.log("Unknown flag \""+name+"\"")
+    }
+  }
+}
+
+MongoClient.connect("mongodb://"+url+":27017/levent", function(err, db) {
+    db.admin().authenticate("levent", "levent", function(err, res) {
         assert.equal(null, err);
         console.log("Connected correctly to server");
         db.collection('events').ensureIndex({"location": "2dsphere"});
         connection = db;
     });
-
-
 });
 
 exports.getConnection = function() {
